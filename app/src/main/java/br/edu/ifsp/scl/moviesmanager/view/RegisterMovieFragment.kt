7ae996e03d9b588f4f5f19386c6d1af73202443b
Erika Raissa Bueno da Silva <erikaaraissaqwe@gmail.com>
2023-12-11 11:45:38 -0300

    Arrumando layout para tema escuro.

--- app/src/main/java/br/edu/ifsp/scl/moviesmanager/view/RegisterMovieFragment.kt
+++ app/src/main/java/br/edu/ifsp/scl/moviesmanager/view/RegisterMovieFragment.kt
@@ -5,9 +5,15 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatActivity
 import br.edu.ifsp.scl.moviesmanager.R
+import br.edu.ifsp.scl.moviesmanager.databinding.FragmentListMovieBinding
+import br.edu.ifsp.scl.moviesmanager.databinding.FragmentRegisterMovieBinding
 
 class RegisterMovieFragment : Fragment() {
+
+
+    private lateinit var fragmentRegisterMovieBinding: FragmentRegisterMovieBinding
 
     companion object {
         fun newInstance() = RegisterMovieFragment()
@@ -18,6 +24,8 @@ class RegisterMovieFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        (activity as? AppCompatActivity)?.supportActionBar?.subtitle = getString(R.string.movie_register)
+
         return inflater.inflate(R.layout.fragment_register_movie, container, false)
     }
 
